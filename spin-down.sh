ae50348e27b1877a8d7f6c8c0cd88b5863da2380
Owen Gage <owengage@gmail.com>
2020-03-21 14:00:25 +0000

    Terminate EC2 instance when spinning down

--- spin-down.sh
+++ spin-down.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-set -ex
+set -e
+
+function upload_failed {
+    echo upload failed
+    echo not terminating EC2 instance.
+    exit 1
+}
 
 instance_id=$(cat .instance_id)
 eip=$(cat .eip)
 eip_id=$(cat .eip_id)
 
-cat <<EOF |  ssh -i minecraft-key-pair.pem ec2-user@$eip
-pkill -f "server.jar"
+cat <<EOF |  ssh -i minecraft-key-pair.pem ec2-user@$eip || upload_failed
+set -e
+echo killing
+pkill -f "server.jar" || true
+echo reuploading 
 ./reupload.sh
 EOF
 
-
-#aws ec2 terminate-instances --instance-id "$instance_id"
+echo terminating EC2 instance
+aws ec2 terminate-instances --instance-id "$instance_id"
