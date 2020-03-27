492ded26b835991a4740dc0cef40c16a3d425068
Owen Gage <owengage@gmail.com>
2020-03-27 10:40:39 +0000

    Get rid of hard-coded region in release script

--- release-server-wrapper.sh
+++ release-server-wrapper.sh
@@ -3,7 +3,7 @@
 ACCOUNT=$(aws sts get-caller-identity | jq -r .Account)
 REGION=$(aws configure get region)
 
-aws ecr get-login-password --region eu-west-2 | \
+aws ecr get-login-password --region $REGION | \
     docker login --username AWS --password-stdin $ACCOUNT.dkr.ecr.$REGION.amazonaws.com/minecloud/server-wrapper
 
 docker build -f server-wrapper.Dockerfile -t minecloud/server-wrapper .
