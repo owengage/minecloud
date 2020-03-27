52d92fb38db50fa44d9189716c81f01fe7510306
Owen Gage <owengage@gmail.com>
2020-03-27 10:32:36 +0000

    Remove old bash scripts.

--- remote-bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-eip=$(cat .eip)
-
-cat bootstrap.sh | ssh \
-    -oStrictHostKeyChecking=accept-new \
-    -i minecraft-key-pair.pem ec2-user@$eip 'bash -'
-
--- remote-stdout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-eip=$(cat .eip)
-
-echo "cat stdout.log" | ssh \
-    -i minecraft-key-pair.pem ec2-user@$eip 'bash -'
-
