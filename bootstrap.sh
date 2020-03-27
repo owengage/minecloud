f0632e1210826a381d606d93f05c110b778b41d9
Owen Gage <owengage@gmail.com>
2020-03-27 10:37:31 +0000

    Remove s3-base and other scripts

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws s3 cp s3://ogage-minecraft/init.sh .
-chmod +x init.sh
-./init.sh
-./run.sh
--- s3-base/reupload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Package server
-tar czvf cliff-side-server.tar.gz cliff-side-server
-
-# Upload
-aws s3 cp cliff-side-server.tar.gz s3://ogage-minecraft/cliff-side-server.tar.gz
-
