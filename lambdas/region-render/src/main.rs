b7e68be5b06d93f4a6aafaf3317d4d830ef45097
Owen Gage <owengage@gmail.com>
2021-04-01 20:32:33 +0100

    update anvil for transparency

--- lambdas/region-render/src/main.rs
+++ lambdas/region-render/src/main.rs
@@ -1,5 +1,5 @@
 use lambda_runtime::{handler_fn, Context};
-use log::{self, LevelFilter};
+use log::{self, error, info, warn, LevelFilter};
 use rusoto_core::{ByteStream, Region};
 use rusoto_s3::{GetObjectRequest, PutObjectRequest, S3Client, S3};
 use tokio::io::AsyncReadExt;
@@ -22,9 +22,16 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
+#[derive(Debug)]
+struct TileDetails<'a> {
+    world: &'a str,
+    dimension: &'a str,
+    x: isize,
+    z: isize,
+}
+
 async fn handler(e: S3Event, _: Context) -> Result<(), Error> {
     let dest_bucket = "owengage.com";
-    let dest_prefix = "anvil-tiles/";
 
     let client = S3Client::new(Region::EuWest2);
     let r = renderer::TileRenderer::new();
@@ -34,34 +41,81 @@ async fn handler(e: S3Event, _: Context) -> Result<(), Error> {
         let key = record.s3.object.key.expect("could not get s3 key");
 
         let re = regex::Regex::new(
-            r"^worlds/(?P<key>[\-0-9A-Za-z_]+/region/r\.[\-0-9]+\.[\-0-9]+)\.mca$",
+            r"^worlds/(?P<world>[\-0-9A-Za-z_]+)/(?P<dimension>[\-0-9A-Za-z_]+)/r\.(?P<x>[\-0-9]+)\.(?P<z>[\-0-9]+)\.mca$"
         )
         .expect("could not compile regex");
 
-        let captures = match re.captures(&key) {
-            Some(cap) => cap,
-            None => return Ok(()), // might be the end/nether. Just ignore for now.
+        let details = match re.captures(&key) {
+            Some(cap) => extract_details(cap),
+            None => {
+                warn!("key not of expected format, skipping: {}", key);
+                return Ok(());
+            }
         };
 
-        let tile_key = dest_prefix.to_string() + captures.name("key").unwrap().as_str() + ".png";
+        info!("tile to process: {:?}", details);
 
-        let obj = client.get_object(GetObjectRequest {
+        let tile_key = format!(
+            "anvil-tiles/{}/{}/r.{}.{}.png",
+            details.world, details.dimension, details.x, details.z,
+        );
+
+        let region_obj = client.get_object(GetObjectRequest {
             bucket: bucket.clone(),
             key: key.clone(),
             ..Default::default()
         });
 
-        let mut buf = vec![];
-        obj.await
+        let mut region = vec![];
+        region_obj
+            .await
             .unwrap()
             .body
             .unwrap()
             .into_async_read()
-            .read_to_end(&mut buf)
+            .read_to_end(&mut region)
             .await
             .expect("could not download region");
 
-        let img = r.render(buf.as_slice());
+        let north_region_obj = client.get_object(GetObjectRequest {
+            bucket: bucket.clone(),
+            key: format!(
+                "worlds/{}/{}/r.{}.{}.mca",
+                details.world,
+                details.dimension,
+                details.x,
+                details.z - 1
+            ),
+            ..Default::default()
+        });
+
+        // TODO: LET IT FAIL.
+        let mut north_region = vec![];
+        let north_req = north_region_obj.await;
+
+        match north_req {
+            Ok(north_req) => {
+                north_req
+                    .body
+                    .expect("could not read north region body")
+                    .into_async_read()
+                    .read_to_end(&mut north_region)
+                    .await
+                    .expect("count not download north region");
+            }
+            Err(err) => {
+                info!(
+                    "could get north region (this is fine if there is no northern region): {:?}",
+                    err
+                );
+            }
+        }
+
+        let img = if north_region.is_empty() {
+            r.render(&region, None)
+        } else {
+            r.render(&region, Some(&north_region))
+        };
 
         client
             .put_object(PutObjectRequest {
@@ -75,4 +129,13 @@ async fn handler(e: S3Event, _: Context) -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+fn extract_details(cap: regex::Captures) -> TileDetails {
+    TileDetails {
+        world: cap.name("world").unwrap().as_str(),
+        dimension: cap.name("dimension").unwrap().as_str(),
+        x: cap.name("x").unwrap().as_str().parse().unwrap(),
+        z: cap.name("z").unwrap().as_str().parse().unwrap(),
+    }
 }
--- lambdas/region-render/src/renderer.rs
+++ lambdas/region-render/src/renderer.rs
@@ -1,7 +1,8 @@
 use std::io::Cursor;
 
 use fastanvil::{
-    parse_region, CCoord, RCoord, RegionBlockDrawer, RegionBuffer, RegionMap, RenderedPalette,
+    CCoord, Chunk, HeightMode, Palette, RCoord, Region, RegionBuffer, RegionMap, RenderedPalette,
+    Rgba, TopShadeRenderer,
 };
 use image::{png::PngEncoder, ColorType};
 
@@ -18,16 +19,15 @@ impl TileRenderer {
         }
     }
 
-    pub fn render(&self, region: &[u8]) -> Vec<u8> {
-        let cursor = Cursor::new(region);
-        let region = RegionBuffer::new(cursor);
+    pub fn render(&self, region: &[u8], north_region: Option<&[u8]>) -> Vec<u8> {
+        let region = RegionBuffer::new(Cursor::new(region));
+        let north_region = north_region.map(|buf| RegionBuffer::new(Cursor::new(buf)));
+        let north_region = north_region.as_ref().map(|r| r as &dyn Region);
 
-        let map = RegionMap::new(RCoord(0), RCoord(0), [0, 0, 0, 0]);
-        let mut drawer = RegionBlockDrawer::new(map, &self.pal);
+        let renderer = TopShadeRenderer::new(&self.pal, HeightMode::Calculate);
 
-        parse_region(&region, &mut drawer).unwrap_or_default(); // TODO handle some of the errors here
+        let region = render_region(&region, north_region, renderer); // TODO handle some of the errors here
 
-        let region = drawer.map;
         let region_len: usize = 32 * 16;
 
         let mut img = image::ImageBuffer::new(region_len as u32, region_len as u32);
@@ -56,4 +56,51 @@ impl TileRenderer {
 
         png
     }
+}
+
+// TODO: Reconcile with the similar method in fastanvil/src/render.rs
+pub fn render_region<P: Palette>(
+    region: &dyn Region,
+    north_region: Option<&dyn Region>,
+    renderer: TopShadeRenderer<P>,
+) -> RegionMap<Rgba> {
+    let mut map = RegionMap::new(RCoord(0), RCoord(0), [0u8; 4]);
+
+    let mut cache: [Option<Box<dyn Chunk>>; 32] = Default::default();
+
+    // Cache the last row of chunks from the above region to allow top-shading
+    // on region boundaries.
+    if let Some(north_region) = north_region {
+        for x in 0..32 {
+            cache[x] = north_region.chunk(CCoord(x as isize), CCoord(31));
+        }
+    }
+
+    for z in 0isize..32 {
+        for x in 0isize..32 {
+            let (x, z) = (CCoord(x), CCoord(z));
+            let data = map.chunk_mut(x, z);
+
+            let chunk_data = region.chunk(x, z).map(|chunk| {
+                // Get the chunk at the same x coordinate from the cache. This
+                // should be the chunk that is directly above the current. We
+                // know this because once we have processed this chunk we put it
+                // in the cache in the same place. So the next time we get the
+                // current one will be when we're processing directly below us.
+                //
+                // Thanks to the default None value this works fine for the
+                // first row or for any missing chunks.
+                let north = cache[x.0 as usize].as_ref().map(|c| &**c);
+                let res = renderer.render(&*chunk, north);
+                cache[x.0 as usize] = Some(chunk);
+                res
+            });
+
+            chunk_data.map(|d| {
+                data[..].clone_from_slice(&d);
+            });
+        }
+    }
+
+    map
 }
